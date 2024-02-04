edaec4743289892bbaafaec9d33564669db2b7e3
juferdinand <70128315+juferdinand@users.noreply.github.com>
2024-02-04 22:55:33 +0100

    Changelog: - Base AuthenticationService Project

---
+++ services/AuthenticationService/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+}
+
+group = "de.juferdinand.financialtracking.app"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.netflix.graphql.dgs:graphql-dgs:8.2.5")
+	implementation("com.netflix.graphql.dgs:graphql-dgs-reactive:8.2.5")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.postgresql:r2dbc-postgresql")
+	implementation("com.auth0:java-jwt:4.4.0")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:r2dbc")
+	testImplementation("org.flywaydb:flyway-core:10.7.1")
+	testImplementation("org.flywaydb:flyway-database-postgresql:10.7.1")
+	testImplementation("org.postgresql:postgresql:42.7.1")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ services/AuthenticationService/src/test/kotlin/de/juferdinand/financialtracking/app/authenticationservice/AuthenticationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.juferdinand.financialtracking.app.authenticationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
